a75d06e93755e19aeb0c2953637b1fa8d73605e0
Morten Berg Nissen <mbn@coinify.com>
2021-05-16 14:33:24 +0200

    Seed for local development

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,25 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+#
+alias Pv.Posts
+alias Pv.Accounts
+
+{:ok, user} =
+  Accounts.register_user(%{
+    "email" => "test@test.dk",
+    "full_name" => "Morten Nissen",
+    "apartment_number" => 18,
+    "password" => "test"
+  })
+
+Enum.each(0..99, fn _x ->
+  Posts.create_post(
+    %{
+      "title" => "Lorem Ipsum",
+      "description" =>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut faucibus metus et dolor volutpat maximus. In luctus odio sed convallis auctor. In hac habitasse platea dictumst. In hac habitasse platea dictumst. Nunc rutrum volutpat purus, ac semper nunc condimentum eget. Morbi dictum dui quis nisi pharetra posuere. Suspendisse vestibulum urna et dapibus mattis. Donec nunc odio, maximus malesuada sodales eu, elementum vel ipsum. Donec ac magna nec ante interdum commodo iaculis sollicitudin ex. Integer aliquam rhoncus velit, quis accumsan nisi egestas eget."
+    },
+    user
+  )
+end)
